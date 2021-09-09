b39708e905700159aa7e9e8550cb6082a345bed3
nto300002 <samonkntd@gmail.co>
2021-09-09 22:01:16 +0900

    練習2

--- Math-Workbook/kadai.js
+++ Math-Workbook/kadai.js
@@ -85,3 +85,100 @@ function Concat(ary1,ary2){
   }
 }
 console.log(Concat(['1','2'],[1,"2"]))
+
+
+
+
+/**
+ *  文字列"あいうえお"を変数で定義する。
+ */
+let a = 'あいうえお';
+console.log(a)
+/**
+ * 文字列"あいうえお"を"かきくけこ"に変更する
+ */
+a = 'かきくけこ'
+console.log(a)
+/**
+ * 文字列"あいうえお"と"かきくけこ"の2つの文字列を連結する。
+ */
+let a1 = 'あいうえお'
+let b1 = 'かきくけこ'
+
+let c1 = a1+b1
+c1 = c1.split('')
+c1 = c1.join('')
+
+console.log(c1)
+console.log(a1.concat(b1))
+/**
+ * 文字列"あいうえお"に含まれる"あ"を"お"に変換する
+ */
+console.log(a1.replace('あ','お'))
+/**
+ * 文字列"あああああ"に含まれる"あ"を"お"に変換する
+ */
+let aa = 'あああああ'
+aa =aa.replaceAll('あ', 'お')
+console.log(aa)
+/**
+ * 文字列"あああああ"に含まれる"あ"を"お"に変換する
+ */
+let aa = 'あああああ'
+aa =aa.replaceAll('あ', 'お')
+console.log(aa)
+/**
+ * 文字列"あいうえお"を1文字ずつ分けて配列に入れる
+ */
+let split = 'あいうえお'
+split.split('')
+
+
+
+
+/**
+ * 空のオブジェクトを定義する
+ */
+const f = {};
+/**
+ * プロパティnameが"ボブ"、ageが20であるオブジェクトを定義する
+ */
+const people={
+  name: 'ボブ',
+  age: 20
+}
+// オブジェクトのnameプロパティの値を取得する
+people.name
+// オブジェクトのageプロパティの値を30に変更する
+people.age = 30;
+people.age
+// オブジェクトにプロパティweightとその値"50kg"を追加する
+people.weight='50kg';
+people.weight
+// オブジェクトのnameプロパティを削除する
+delete people.name
+console.log(people)
+
+
+
+// 空の配列を定義する
+let air = []
+/**
+ * 文字列要素"りんご"、"みかん"、"いちご"、"もも"が入った配列を定義する
+ */
+let fruit = ["りんご","みかん","いちご","もも"]
+// 配列の2番目の要素を参照する
+fruit[1];
+// 配列の3番目の要素を変更する
+fruit[2] = 'かぼす';
+// 配列の最後に文字列要素"すいか"を追加する
+fruit.push('すいか')
+// 配列の最後の要素を削除する
+fruit.pop()
+// 配列の最初に文字列要素"すいか"を追加する
+fruit.unshift('すいか')
+// 配列の最初の要素を削除する
+fruit.shift()
+// 配列を逆順にする
+fruit.reverse()
+console.log(fruit)
