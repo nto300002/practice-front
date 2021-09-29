79263d2bce4bbc8ba12e78ba2589762932e6dbcd
nto300002 <samonkntd@gmail.co>
2021-09-29 20:44:25 +0900

    text

---
+++ user-admin/src/App.tsx
@@ -0,0 +1,17 @@
+import { Button, ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./router/Router";
+import theme from "./theme/theme";
+
+
+export default function App() {
+  return (
+    <div className="App">
+      <ChakraProvider theme={theme}>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </ChakraProvider>
+    </div>
+  );
+}
